325ab2af1870e3eeee9334dbe752ea09c95cb915
Maynor de Leon <maynoralex@gmail.com>
2025-07-27 22:51:57 +0200

    use Reselect for memoization and avoid unnecesary re-rendering

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,7 +1,20 @@
+import { createSelector } from 'reselect'
 
-export const selectCategoriesMap = (state) => state.categories.categories.reduce((acc, category)=>{
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer], //input selector or slice of the reducer
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        return categories.reduce((acc, category)=>{
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc; 
-    },{})
-;
+        },{});
+    }
+);
+
