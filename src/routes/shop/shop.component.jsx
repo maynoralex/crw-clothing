28eac11753be8296bc32e015ddc8994827946d0e
Maynor de Leon <maynoralex@gmail.com>
2025-07-30 21:16:27 +0200

    Use Redux Thunk to handle await calls to fetchCategories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,20 +4,14 @@ import { Route, Routes } from 'react-router'
 import './shop.styles.scss'
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
-import { getCollectionAndDocuments } from '../../utils/firebase/firebase'
-import { setCategories } from '../../store/category/category.action'
+import { fetchCategoriesAsync } from '../../store/category/category.action'
 
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-            const getCategoriesMap = async () => {
-                const categoriesArray = await getCollectionAndDocuments('categories');
-                dispatch(setCategories(categoriesArray));
-            }
-    
-            getCategoriesMap();
+            dispatch(fetchCategoriesAsync());
     
         },[]);
 
--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,6 +1,26 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
+import { getCollectionAndDocuments } from '../../utils/firebase/firebase'
 
 
-export const setCategories = (categoriesArray) => 
-    createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray)
+export const fetchCategoriesStart = () => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailed = (error) => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+    
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    
+    try{
+        const categoriesArray = await getCollectionAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+        
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error))
+    }
+}
